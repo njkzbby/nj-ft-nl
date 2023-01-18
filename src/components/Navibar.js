7464b91c2a1fb61aceffff632804172cea1eb795
njkzbby <njkzbby@yandex.ru>
2023-01-19 02:13:16 +0500

    update navibar

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, Link, Button, Form } from 'react-bootstrap';
+import { Navbar, Nav, Button, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Navibar.scss';
 
 
@@ -10,7 +10,21 @@ export default function NaviBar() {
     return (
         <>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand className="LogoName">NJFTNL</Navbar.Brand>
+                <Container fluid>
+                    <Navbar.Brand href="#" className='LogoName' >NJFTNL</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className={["me-auto", "font-large"]}>
+                            <Nav.Link><Link to="/"> Home </Link></Nav.Link>
+                            <Nav.Link><Link to="/users"> Users </Link></Nav.Link>
+                            <Nav.Link><Link to="/about"> About </Link></Nav.Link>
+                        </Nav>
+                        <Nav className='justify-content-end'>
+                            <Button className='btn-custom me-2'>Log In</Button>
+                            <Button className='btn-custom'>Log Out</Button>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
         </>
     )
