3be03801a2fa5d029999c6c78471cc829a93b78c
njkzbby <njkzbby@yandex.ru>
2023-01-19 02:18:13 +0500

    add routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'bootstrap';
-import { ReactDOM } from 'react';
 import NaviBar from './components/Navibar';
-import { Navbar, Nav, Link } from 'bootstrap';
+import { Home } from './components/Home';
+import { Users } from './components/Users';
+import { About } from './components/About';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 
 
 function App() {
   return (
     <div className="App">
-      <NaviBar></NaviBar>
+
+      <Router>
+        <NaviBar></NaviBar>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="*"
+            element={<Navigate to="/" replace />}
+          />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
