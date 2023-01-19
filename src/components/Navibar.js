87ca2738e0475124f0c456236d755887dd9c5e78
njkzbby <njkzbby@yandex.ru>
2023-01-20 01:20:11 +0500

    add login form and update navbar

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, Button, Container } from 'react-bootstrap';
+import { Navbar, Nav, Button, Container, Modal, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Navibar.scss';
 import styled from 'styled-components';
@@ -15,6 +16,12 @@ const Styles = styled.div`
 `
 
 export default function NaviBar() {
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
     return (
         <>
             <Styles>
@@ -29,13 +36,35 @@ export default function NaviBar() {
                                 <Nav.Link><Link to="/about"> About </Link></Nav.Link>
                             </Nav>
                             <Nav className='justify-content-end'>
-                                <Button className='btn-custom me-2'>Log In</Button>
-                                <Button className='btn-custom'>Log Out</Button>
+                                <Button className='btn-custom me-2' onClick={handleShow}>Log In</Button>
+                                <Button className='btn-custom' onClick={handleShow}>Log Out</Button>
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
             </Styles>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Log in</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId='fromBasicEmail'>
+                            <Form.Label>Email Adress</Form.Label>
+                            <Form.Control type='email' placeholder='Enter email' />
+                            <Form.Text className='text-muted'>We'll never share your email with anyone else.</Form.Text>
+                        </Form.Group>
+                        <Form.Group controlId='fromBasicPassword'>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type='password' placeholder='Enter password' />
+                        </Form.Group>
+                        <Form.Group controlId='fromBasicCheckbox'>
+                            <Form.Check type="checkbox" label="Remember me" />
+
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+            </Modal>
         </>
     )
 }
