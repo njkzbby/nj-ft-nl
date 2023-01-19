fda72a0cee569e303ecafdbd13571d4ecf907e5e
njkzbby <njkzbby@yandex.ru>
2023-01-20 00:28:28 +0500

    update navbar

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -3,29 +3,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, Button, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Navibar.scss';
+import styled from 'styled-components';
 
-
+const Styles = styled.div`
+ a, .navbar-brand, .navbar-nav, .nav-link {
+    color: #adb1b8;
+    &hover{
+        color: white
+    }
+ }
+`
 
 export default function NaviBar() {
     return (
         <>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Container fluid>
-                    <Navbar.Brand href="#" className='LogoName' >NJFTNL</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className={["me-auto", "font-large"]}>
-                            <Nav.Link><Link to="/"> Home </Link></Nav.Link>
-                            <Nav.Link><Link to="/users"> Users </Link></Nav.Link>
-                            <Nav.Link><Link to="/about"> About </Link></Nav.Link>
-                        </Nav>
-                        <Nav className='justify-content-end'>
-                            <Button className='btn-custom me-2'>Log In</Button>
-                            <Button className='btn-custom'>Log Out</Button>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+            <Styles>
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                    <Container >
+                        <Navbar.Brand href="#" className='LogoName' >NJFTNL</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className={["me-auto", "font-large"]}>
+                                <Nav.Link><Link to="/"> Home </Link></Nav.Link>
+                                <Nav.Link><Link to="/users"> Users </Link></Nav.Link>
+                                <Nav.Link><Link to="/about"> About </Link></Nav.Link>
+                            </Nav>
+                            <Nav className='justify-content-end'>
+                                <Button className='btn-custom me-2'>Log In</Button>
+                                <Button className='btn-custom'>Log Out</Button>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </Styles>
         </>
     )
 }
